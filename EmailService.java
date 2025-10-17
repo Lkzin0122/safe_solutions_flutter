e3700a615f9b1340492b74819272ab80792de3fa
Lkzin0122 <bitencourtes@gmail.com>
2025-10-17 08:54:08 -0300

    Alterações: exclusão do JAVA

--- EmailService.java
+++
@@ -1,5 +0,0 @@
-package br.com.itb.miniprojetospring.service;
-
-public interface EmailService {
-    void enviarEmailRecuperacao(String email, String nomeUsuario, String token);
-}
--- EmpresaService_UPDATE.java
+++
@@ -1,5 +0,0 @@
-// Adicione este método na classe EmpresaService
-
-public Optional<Empresa> findByCnpj(String cnpj) {
-    return empresaRepository.findById(cnpj);
-}
